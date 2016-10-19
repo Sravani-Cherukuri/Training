1c159ddec07be28eee9765cf091c83bb8c484067
lnc <lnc@DIN76000530.corp.capgemini.com>
2016-10-19 16:01:42 +0530

    first commit

---
+++ DemoProject/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,14 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Sales sale=new Sales();
+		sale.display();
+		greetings();
+	}
+	public static void greetings(){
+		System.out.println("Hello welcome to main");
+	}
+
+}
---
+++ DemoProject/src/main/java/org/cap/demo/Sales.java
@@ -0,0 +1,8 @@
+package org.cap.demo;
+
+public class Sales {
+
+	public void display(){
+		System.out.println("This is display from Sales");
+	}
+}
